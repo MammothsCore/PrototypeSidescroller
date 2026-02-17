0af5ec9df9576247573d2b89570f5babc9be16c6
Duefix193 <roland.bobretzky@yahoo.de>
2026-02-17 10:27:13 +0100

    and healthbar enemy

--- Assets/Scripts/Enemy/Enemy_Health.cs
+++ Assets/Scripts/Enemy/Enemy_Health.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy_Health : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
+    public Slider slider;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = currentHealth;
+    }
+
+    void Update()
+    {
+        slider.value = currentHealth;
     }
 
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+        slider.value = currentHealth;
 
         if (currentHealth > maxHealth)
         {
             currentHealth = maxHealth;
+            slider.value = currentHealth;
         }
         else if (currentHealth <= 0)
         {
